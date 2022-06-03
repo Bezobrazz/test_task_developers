f420577b87b553cd8b4f92d4d0b03e4465e0b4b1
Bezobrazz <kostenko.uvd@gmail.com>
2022-06-03 19:19:05 +0300

    add accordion section and mobile adaptation + js

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,20 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// Accordion-----------------------------
+let acc = document.getElementsByClassName("accordion__btn");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
