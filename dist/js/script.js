a72223fee866c5abd37a9929d1fe3b6e7ca40ce9
Bezobrazz <kostenko.uvd@gmail.com>
2022-06-08 18:19:40 +0300

    add sections: example, information, form

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,4 +39,57 @@ $(document).ready(function () {
     slidesToShow: 2,
     variableWidth: true,
   });
+
+  // Example slider-----------------------------------
+  // let position = 0;
+  // const slidesToShow = 1;
+  // const slidesToScroll = 1;
+  // const container = $(".example__wrapper");
+  // const track = $(".example__track");
+  // const item = $(".example__icon");
+  // const btnPrev = $(".example__btn_prev");
+  // const btnNext = $(".example__btn_next");
+  // const itemWidth = container.width() / slidesToShow;
+  // const movePosition = slidesToScroll * itemWidth;
+
+  // item.each(function (index, item) {
+  //   $(item).css({
+  //     minWidth: itemWidth,
+  //   });
+  // });
+
+  // btnPrev.click(function () {
+  //   position += movePosition;
+  //   track.css({
+  //     transforme: `translateX(${movePosition}px)`,
+  //   });
+  // });
+  // btnNext.click(function () {
+  //   position -= movePosition;
+  //   track.css({
+  //     transforme: `translateX(${movePosition}px)`,
+  //   });
+  // });
+  let offset = 0;
+  const sliderShop = document.querySelector(".example__track");
+
+  document
+    .querySelector(".example__btn_next")
+    .addEventListener("click", function () {
+      offset += 350;
+      if (offset > 350) {
+        offset = 0;
+      }
+      sliderShop.style.left = -offset + "px";
+    });
+
+  document
+    .querySelector(".example__btn_prev")
+    .addEventListener("click", function () {
+      offset -= 350;
+      if (offset < 0) {
+        offset = 350;
+      }
+      sliderShop.style.left = -offset + "px";
+    });
 });
